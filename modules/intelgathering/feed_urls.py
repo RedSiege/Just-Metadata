b9543f1703bed99046d0d2351f8d9ffb36d7cea7
nicolasvillatte <nicolas.villatte@gmail.com>
2016-01-12 22:14:01 +0100

    Update feed_urls.py

--- modules/intelgathering/feed_urls.py
+++ modules/intelgathering/feed_urls.py
@@ -64,8 +64,7 @@ class IntelGather:
             req = urllib2.Request(
                 'http://reputation.alienvault.com/reputation.data')
             req.add_header(
-                'User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.
-0.2454.101 Safari/537.36')
+                'User-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36')
             response = urllib2.urlopen(req)
             alientvault_resp = response.read()
         except NameError:
