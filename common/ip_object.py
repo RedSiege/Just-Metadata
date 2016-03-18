22947fdb03ef60166fc117d1568c8b40d013b643
Xavier Mertens <xavier@rootshell.be>
2016-03-18 15:19:08 +0100

    Added dshield info

--- common/ip_object.py
+++ common/ip_object.py
@@ -46,6 +46,10 @@ class IP_Information:
         self.malc0de = ""
         self.malwarebytes = ""
         self.mywot = ""
+	self.dshield_count = ""
+	self.dshield_attacks = ""
+	self.dshield_maxdate = ""
+	self.dshield_mindate = ""
 
         if self.check_ip(incoming_system):
             self.ip_address = incoming_system
